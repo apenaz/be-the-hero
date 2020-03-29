1cf8d1ae18960ede7c64aca776fb85d92308e18f
Patricio Oliveira <patricio@bct.ect.ufrn.br>
2020-03-29 17:06:31 -0300

    backend da aplicação

---
+++ backend/src/controllers/ProfileController.js
@@ -0,0 +1,17 @@
+const connection = require('../database/connection');
+
+module.exports = {
+    async index(request, response) {
+        
+        const ong_id = request.headers.authorization;
+        const incidents = await connection('incidents')
+        .where('ong_id', ong_id)
+        .select('*');
+
+        if (incidents == '') {
+            return response.json({msg: 'Not incidents in database'})            
+        }
+        
+        return response.json(incidents);
+    }
+}
